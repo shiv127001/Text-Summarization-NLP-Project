aa2e6d9d48945a9aa8b9824830c6a6cf1bb20aa3
shiv127001 <shiv127001@gmail.com>
2025-05-31 22:00:31 +0530

    setup.py and main.py error solved

--- setup.py
+++ setup.py
@@ -3,28 +3,30 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarization-NLP-Project"
 AUTHOR_USER_NAME = "shiv127001"
-SRC_REPO = "textSummarizer"
+PACKAGE_NAME = "textSummarizer"
 AUTHOR_EMAIL = "shiv127001@gmail.com"
 
-
-
 setuptools.setup(
-    name=SRC_REPO,
+    name=PACKAGE_NAME,  # ✅ FIXED: Use package name, not path
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # ✅ FIXED: this was "text/markdown"
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
